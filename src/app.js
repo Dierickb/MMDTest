5159a2de478b2799b92e4d1556b1f1c18a090fdb
Dierickb <71480347+Dierickb@users.noreply.github.com>
2022-06-08 15:16:04 -0500

    fix: order app settings

--- src/app.js
+++ src/app.js
@@ -9,31 +9,29 @@ require("dotenv").config();
 const app = express();
 
 // settings
-app.use(
-    session({
-        secret: "secret",
-        resave: true,
-        saveUninitialized: true,
-    })
-);
 app.engine("ejs", engine);
 app.set("port", process.env.WEBPORT || 80);
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 // Middlewares
-app.use(express.json());
+app.use(
+    session({
+        secret: "secret",
+        resave: true,
+        saveUninitialized: true,
+        cookie: {
+            maxAge: 1000 * 60 * 60 * 5 // Equals 5 hours
+        }
+    })
+);
 app.use(morgan("dev"));
-app.use(express.urlencoded({ extended: false }));
-
-// Global Variables
-
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 //Routes
 const indexRouter = require('./routes/index.routes');
 const indexApi = require('./routes/api/index')
-
 app.use('/', indexRouter);
 app.use('/api/v1', indexApi)
 
